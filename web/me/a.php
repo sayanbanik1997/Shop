39a66b40fd6c39da68d1518fda90e9c20458deaa
alexP <sayanbanik1997@gmail.com>
2024-11-23 15:53:28 +0530

    first commit

---
+++ web/me/a.php
@@ -0,0 +1,8 @@
+<?php
+    if(isset($_POST['data'])){
+        //$json= json_decode($_POST['data']);
+        //$name = $json[0]['name'];
+        //echo $name;
+        echo $_POST['data'];
+    }
+?>
