b2d42a0e6f7aebf8a3bc9b9aa5b1c730aa43d8ae
alexP <sayanbanik1997@gmail.com>
2024-11-24 19:18:36 +0530

    test

--- app/src/main/java/com/google/sayanbanik1997/shop/MainActivity.java
+++ app/src/main/java/com/google/sayanbanik1997/shop/MainActivity.java
@@ -3,13 +3,21 @@ package com.google.sayanbanik1997.shop;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
+    NavigationView navView;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +28,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        toolbar=(androidx.appcompat.widget.Toolbar) findViewById(R.id.toolBar);
+        drawerLayout=(DrawerLayout) findViewById(R.id.main);
+        actionBarDrawerToggle= new ActionBarDrawerToggle(this, drawerLayout, toolbar, 0, 1);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        navView=(com.google.android.material.navigation.NavigationView) findViewById(R.id.navView);
     }
 }
