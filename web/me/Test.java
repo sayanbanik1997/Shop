532fb168b239c8fd0cf5b84514d31e1fefe0f24b
alexP <sayanbanik1997@gmail.com>
2024-12-02 16:22:00 +0530

    test

--- web/me/Test.java
+++ web/me/Test.java
@@ -1,10 +1,5 @@
 class Test{
-    final int b;
-    final int a(int a){
-        return a;
-    }
     public static void main(String[] args) {
-        final Test t=new Test();
-        System.out.println(t.a(7));
+        System.out.println(100.1%10);
     }
 }
